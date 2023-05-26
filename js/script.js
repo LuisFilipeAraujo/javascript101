0d29e15932f64944d6358da4e06e5411783c6fdb
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2023-05-26 15:31:14 -0300

    function calculaFatorial() has changet to fatorial and was reworked to be more efficient

--- js/script.js
+++ js/script.js
@@ -208,3 +208,19 @@ function gasolinaOuEtanol(){
         JanelaPopUp.document.write(`<h1>Tanto faz abastecer com Gasolina ou Etanol!</h1>`);
     }
 }
+
+function fatorial() {
+    let saida = document.getElementById('saida')
+    let n = Number(document.getElementById('fnum').value)
+
+    saida.innerHTML += `<h2>Calculando ${n}!</h2>`
+    let c = n
+    let fat = 1
+    while (c > 1) {
+        saida.innerHTML += `${c} x `
+        fat *= c // Correspondente ao cálculo fat = fat * c
+        c --
+    }
+    saida.innerHTML += `1 = <strong>${fat.toLocaleString('pt-BR')}</strong>`
+    // Usei o toLocaleString() na linha acima apenas para aparecerem os separadores de milhar/milhões/etc...
+}
